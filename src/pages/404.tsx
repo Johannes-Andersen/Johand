bdd56bf65655251813785d89e7706e4ff5c76495
Johannes Andersen <johannes.andersen@signifyd.com>
2021-06-14 20:35:47 +0100

    feat(ErrorPages): add error page skeleton

---
+++ src/pages/404.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react'
+
+const FourOhFourPage: FC = () => <h1>404 - Page Not Found</h1>
+
+export default FourOhFourPage
---
+++ src/pages/500.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react'
+
+const FiveHundredPage: FC = () => <h1>500 - Server-side error occurred</h1>
+
+export default FiveHundredPage
