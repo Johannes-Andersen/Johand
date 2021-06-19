e627c0583d27c6eef0a0ce1358d5673935efedeb
Johannes Andersen <johannes.andersen@signifyd.com>
2021-06-19 15:40:28 +0100

    feat(securityHeaders): add security headers

--- next.config.js
+++ next.config.js
@@ -2,7 +2,59 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 const { i18n } = require('./next-i18next.config')
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Permissions-Policy',
+    value:
+      'gyroscope=(), payment=(), usb=(), battery=(), ambient-light-sensor=(), accelerometer=(), camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'Cross-Origin-Embedder-Policy',
+    value: 'require-corp',
+  },
+  {
+    key: 'Cross-Origin-Opener-Policy',
+    value: 'same-origin',
+  },
+]
+
 module.exports = withPWA({
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+      {
+        source: '/',
+        headers: securityHeaders,
+      },
+    ]
+  },
   poweredByHeader: false,
   reactStrictMode: true,
   pwa: {
