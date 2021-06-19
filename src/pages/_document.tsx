88371d81ee245eea1052727edc74d7010fe25adc
Johannes Andersen <johannes.andersen@signifyd.com>
2021-06-19 21:38:01 +0100

    fix(_document): fix broken PWA meta HTML tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,10 +31,6 @@ class MyDocument extends Document {
             content="Everything you need to know about Johannes, aka. Johand."
           />
           <meta name="mobile-web-app-capable" content="yes" />
-          <meta
-            name="msapplication-config"
-            content="/icons/browserconfig.xml"
-          />
           <meta name="msapplication-TileColor" content="#1a202c" />
           <meta name="msapplication-tap-highlight" content="no" />
           <meta name="theme-color" content="#1a202c" />
@@ -42,38 +38,27 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" href="/images/logo-maskable-512.png" />
           <link
             rel="apple-touch-icon"
-            sizes="152x152"
-            href="/icons/touch-icon-ipad.png"
+            sizes="512x152"
+            href="/images/logo-512.png"
           />
           <link
             rel="apple-touch-icon"
-            sizes="180x180"
-            href="/icons/touch-icon-iphone-retina.png"
+            sizes="256x256"
+            href="/images/logo-256.png"
           />
           <link
             rel="apple-touch-icon"
-            sizes="167x167"
-            href="/icons/touch-icon-ipad-retina.png"
+            sizes="128x128"
+            href="/images/logo-128.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="64x64"
+            href="/images/logo-64.png"
           />
 
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/icons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/icons/favicon-16x16.png"
-          />
           <link rel="manifest" href="/manifest.json" />
-          <link
-            rel="mask-icon"
-            href="/icons/safari-pinned-tab.svg"
-            color="#1a202c"
-          />
+          <link rel="mask-icon" href="/images/logo.svg" color="#1a202c" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:url" content="https://johand.dev" />
@@ -99,14 +84,6 @@ class MyDocument extends Document {
             property="og:image"
             content="https://yourdomain.com/icons/apple-touch-icon.png"
           />
-          {/*
-<link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' /> */}
         </Head>
         <body>
           <Main />
