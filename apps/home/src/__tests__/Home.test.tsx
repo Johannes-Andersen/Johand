25b87b19e788c40d0d37f5f9392ff7bf7de7b518
Johannes Andersen <johannes.andersen@signifyd.com>
2022-01-21 19:48:39 +0000

    Fix failing tests

--- apps/home/src/__tests__/Home.test.tsx
+++ apps/home/src/__tests__/Home.test.tsx
@@ -4,7 +4,7 @@ import Home from '../pages/index'
 describe('Home Page', () => {
   it('should render', async () => {
     const { getByText } = render(<Home />)
-    const title = getByText('Johand.dev')
+    const title = getByText('Johand')
     expect(title).toBeInTheDocument()
   })
 })
